e563e3f36ae1befed13d3235909004bcc9a4f556
RenanCescon <renancescon015@gamil.com>
2023-09-22 18:47:14 -0300

    adicionando menu

---
+++ src/Menu.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+
+public class Menu {
+    private Campeonato camp = new Campeonato();
+    private Scanner sc = new Scanner(System.in);
+
+    public void iniciarMenu() {
+        System.out.println("Opções do menu:");
+        System.out.println("a - Incluir jogador.");
+        System.out.println("b - Remover jogador.");
+        System.out.println("c - Iniciar campeonato.");
+        System.out.println("d - Mostrar a cartela do campeonato");
+        System.out.println("e - Gravar campeonato em arquivo.");
+        System.out.println("f - Ler campeonato em arquivo.");
+        System.out.println("g - Sair da aplicação.");
+    }
+
+    public void opcoes(char c) {
+
+        String nome = new String();
+
+        switch(c) {
+            case 'a':
+                camp.incluirJogador();
+                break;
+            case 'b':
+                camp.excluirJogador();
+                break;
+            case 'c':
+                camp.iniciarCampeonato();
+                break;
+            case 'd':
+                camp.mostrarCartela();
+                break;
+            case 'e':
+                break;
+            case 'f':
+                break;
+        }
+    }
+}
